04619de2e3d2841820db2c8a8a0599017e1faf16
MinhBlues <nevergone1203@gmail.com>
2020-12-13 01:43:58 +0700

    fix socket

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './helpers';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,5 +15,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
+serviceWorker.unregister();
 
