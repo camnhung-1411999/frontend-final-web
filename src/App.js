2a674b706202d222cd9668560fbe396450dbad30
nguyennguyen <nguyenhoantuyetnhi@gmail.com>
2020-12-16 16:51:29 +0700

    done page history

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import Home from "./page/auth/Home";
-import Admin from "./page/Admin/Admin";
+import Admin from "./page/admin/Admin";
 import { Router, Switch, Route } from "react-router-dom";
 // import Profile from "./page/Profile";
-import {DashBoard} from "./page/DashBoard";
+import {DashBoard} from "./page/dashboard";
 import {PrivateRoute, NormalRoute} from './components';
 import React from "react";
 import PageNotFound from "./page/error/PageNotFound";
 import {ChessBoard} from "./page/board";
 import {Redirect} from "react-router-dom";
 import {Account} from "./page/account"
+import {HistoryBoard} from "./page/historyboard";
 import { history } from './helpers';
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
               <PrivateRoute path="/home" component={DashBoard} />
               <PrivateRoute path="/board/:id" component={ChessBoard} />
               <PrivateRoute path="/profile" component={Account} />
+              <PrivateRoute path="/historyboard/:user" component={HistoryBoard} />
 
               <Route path="/404" component={PageNotFound} />
               <Redirect from='*' to='/404' />
