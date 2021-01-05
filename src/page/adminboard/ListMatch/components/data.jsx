87df23034c9f4694f8f86d94be2fb2ebad677bea
nguyennguyen <nguyenhoantuyetnhi@gmail.com>
2021-01-05 23:33:55 +0700

    edit ui list match

--- src/page/adminboard/ListMatch/components/data.jsx
+++ src/page/adminboard/ListMatch/components/data.jsx
@@ -3,50 +3,50 @@ import { v4 as uuid } from 'uuid';
 export default [
   {
     id: uuid(),
-    createdAt: '27/03/2019',
+    createdAt: '01-01-2020 16:41:30',
     description: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    media: '/static/images/products/product_1.png',
-    title: 'Dropbox',
+    media: '../../../../assets/image/match.png',
+    title: '1000',
     totalDownloads: '594'
   },
   {
     id: uuid(),
-    createdAt: '31/03/2019',
+    createdAt: '20-12-2020 16:41:30',
     description: 'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
     media: '/static/images/products/product_2.png',
-    title: 'Medium Corporation',
+    title: '1001',
     totalDownloads: '625'
   },
   {
     id: uuid(),
-    createdAt: '03/04/2019',
+    createdAt: '20-12-2020 16:41:30',
     description: 'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
     media: '/static/images/products/product_3.png',
-    title: 'Slack',
+    title: '1010',
     totalDownloads: '857'
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
+    createdAt: '20-12-2020 16:41:30',
     description: 'Lyft is an on-demand transportation company based in San Francisco, California.',
     media: '/static/images/products/product_4.png',
-    title: 'Lyft',
+    title: '1111',
     totalDownloads: '406'
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
+    createdAt: '20-12-2020 16:41:30',
     description: 'GitHub is a web-based hosting service for version control of code using Git.',
     media: '/static/images/products/product_5.png',
-    title: 'GitHub',
+    title: '1112',
     totalDownloads: '835'
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
+    createdAt: '20-12-2020 16:41:30',
     description: 'Squarespace provides software as a service for website building and hosting. Headquartered in NYC.',
     media: '/static/images/products/product_6.png',
-    title: 'Squarespace',
+    title: '1113',
     totalDownloads: '835'
   }
 ];
