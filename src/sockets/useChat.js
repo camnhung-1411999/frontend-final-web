bff63869e18aa1e59239b64ab3e66822ad8afbb7
minhblues <nevergone1203@gmail.com>
2021-01-09 00:08:51 +0700

    update send message

--- src/sockets/useChat.js
+++ src/sockets/useChat.js
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import { socket } from "../helpers";
 const SEND = "sendMessage";
 const RECEIVE = "recievedMessage";
+const JOINROOM = "joinRoom";
 
 
 const useChat = (roomId) => {
     const [messages, setMessages] = useState([]);
+    const username = JSON.parse(localStorage.getItem("username"));
     useEffect(() => {
+        socket.on(JOINROOM, (room)=>{
+            setMessages(room.chat)
+        })
         socket.on(RECEIVE, (message) => {
             const incomingMessage = {
                 ...message,
@@ -22,7 +27,7 @@ const useChat = (roomId) => {
     const sendMessage = (message) => {
         setMessages((messages) => [...messages, { message: message, ownl: true }]);
         socket.emit(SEND, {
-            body: { message: message, ownl: false },
+            body: { message: message, ownl: false, username: username },
             roomId: roomId,
         });
     };
